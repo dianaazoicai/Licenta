9a5f4f0eef87caf9e2a019741e148cb8812a0904
dianaazoicai <61265446+dianaazoicai@users.noreply.github.com>
2021-08-25 11:03:54 +0300

    Add files via upload

--- Frontend/app.js
+++ Frontend/app.js
@@ -3,10 +3,9 @@
 const fs = require('fs');
 const express =require('express');
 const expressLayouts= require('express-ejs-layouts');
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const app=express();
-
 const port =2201;
 
 // directorul 'views' va conține fișierele .ejs (html + js executat la server)
@@ -19,6 +18,7 @@ app.use(express.static('public'))
 app.use(bodyParser.json());
 // utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
 app.use(bodyParser.urlencoded({extended:true}));
+
 let listaPreturi;
 fs.readFile('Preturi.json',(err, data)=>{
     if(err) throw err;
@@ -34,18 +34,64 @@ app.get('/',(req,res)=>res.redirect('/acasa'));
 app.get('/acasa',(req,res)=>{
     res.render("acasa");
 });
-app.get('/programare',(req,res)=>{
-    res.render("formular");
+app.get('/programare',async (req,res)=>{
+    //iau toti medicii din baza de date pt a lua specializarile lor
+    const url= "http://localhost:8080/api/doctors";
+    const settings = {
+        method:'GET',
+        headers:{'Content-Type': 'application/json'}
+    };
+    const medici = await((await fetch(url,settings)).json());
+    res.render("formular",{medici:medici});
 });
-app.get('/servicii',(req,res)=>{
+app.post('/adauga-programare',async(req,res)=>{
+    let date=JSON.stringify(req.body);
+    console.log(req.body);
+    date=JSON.parse(date);
+    const url1= "http://localhost:8080/api/pacients/"+date.CNP;
+    const settings1 = {
+        method:'GET',
+        headers:{'Content-Type': 'application/json'}
+    };
+    const pacienti = await((await fetch(url1,settings1)).json());
+    if(pacienti.length==0)
+    {
+        res.sendStatus(404);
+        return;
+    }
+    let bdy=JSON.stringify({id_programare: 1, id_doctor:date.specializare,id_pacient:pacienti[0].id_pacient, data_programare:date.programare})
+
+
+    const url= "http://localhost:8080/api/programari/add";
+    const settings = {
+        method:'POST',
+        headers:{'Content-Type': 'application/json'},
+        body:bdy
+    };
+    const rawResponse =await((await fetch(url,settings)).json());
+    console.log(rawResponse);
+    res.render('confirmareProgramare')
+
+})
+app.get('/servicii', (req,res)=>{
     res.render("servicii");
 });
-app.get('/medici',(req,res)=>{
-    res.render("medici");
+app.get('/medici',async(req,res)=>{
+
+    const url= "http://localhost:8080/api/doctors";
+    const settings = {
+        method:'GET',
+        headers:{'Content-Type': 'application/json'}
+    };
+    const medici = await((await fetch(url,settings)).json());
+    res.render("medici",{medici:medici});
 });
 app.get('/autentificare',(req,res)=>{
     res.render("autentificare");
 });
+app.post('/verificare-autentificare',(req,res)=>{
+    res.render("formular");
+})
 app.get('/contact',(req,res)=>{
     res.render("contact");
 });
@@ -56,10 +102,29 @@ app.get('/termeni',(req,res)=>{
     res.render("termeni");
 });
 
-app.post('/test',(req,res)=>{
-    
 
+app.get('/inregistrare',(req, res) => {
+    res.render('inregistrare');
+    
 });
+app.post('/adauga_client',(req,res)=>{
+    let dataToInsert=JSON.stringify(req.body);
+    var data=JSON.parse(dataToInsert)
+    let bdy=JSON.stringify({id_pacient:1,nume_pacient:data.nume,CNP:data.cnp,telefon:data.telefon,adresa:data.adresa,email:data.email})
+    const url= "http://localhost:8080/api/pacients/add";
+    const settings = {
+        method:'POST',
+        headers:{'Content-Type': 'application/json'},
+        body:bdy
+       };
+    (async () => {
+        const rawResponse = await fetch(url,settings);
+        const content = await rawResponse.json();
+      
+      })();
+      res.render('autentificare');
+});
+
 
 
 
